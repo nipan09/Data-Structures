1488daaba7b80f6060094abef60161aa6428dff5
nipan09 <pandey.nishant489@gmail.com>
2021-05-20 23:34:21 +0530

    program re-structured

--- graph/graph-adj-list.cpp
+++ graph/graph-adj-list.cpp
@@ -1,28 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void addEdge(vector<int>adj[], int u, int v){
-	adj[u].push_back(v);
-	adj[v].push_back(u);
+struct Edge{
+	int src, dest;
+};
+
+class Graph{
+	public:
+	vector<vector<int>> adjList;
+	
+	Graph(vector<Edge> &edges, int N){
+		adjList.resize(N);
+		
+		for(auto &edge: edges)
+			adjList[edge.src].push_back(edge.dest);
+	}
+};
+	
+void printGraph(Graph &graph, int N){
+	for(int i=0; i<N; i++){
+		cout<<i<<"-> ";
+		
+		for(int v:graph.adjList[i]) cout<<v<<" ";
+		
+		cout<<endl;
+	}
 }
 
 int main(){
-	int v=5;
-	vector<int> adj[v];
-	addEdge(adj, 0, 1);
-	addEdge(adj, 0, 4);
-	addEdge(adj, 1, 2);
-	addEdge(adj, 1, 3);
-	addEdge(adj, 1, 4);
-	addEdge(adj, 2, 3);
-	addEdge(adj, 3, 4);
-	
-	for(int i=0; i<v; i++){
-		cout<<"\n Adjacency list of vertex "<< i << "\n head";
-		for(auto x: adj[i]) cout<<"-> "<<x;
-		cout<<endl;
-	}
-	
-	return 0;
+    vector<Edge> edges =
+    {
+        { 0, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
+        { 3, 2 }, { 4, 5 }, { 5, 4 }
+    };
+
+    int N = 6;
+ 
+    Graph graph(edges, N);
+ 
+    printGraph(graph, N);
+ 
+    return 0;
 }
-	
